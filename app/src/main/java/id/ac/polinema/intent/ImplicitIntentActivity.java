0e4b7f21e518469f86123f61b758bb1dde3f2fa9
hanifjauhari <hanifjauhar99@gmail.com>
2020-02-21 10:48:17 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/polinema/intent/ImplicitIntentActivity.java
@@ -0,0 +1,37 @@
+package id.ac.polinema.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImplicitIntentActivity extends AppCompatActivity {
+
+    private static final String TAG = ImplicitIntentActivity.class.getCanonicalName();
+    private static final int GALLERY_REQUEST_CODE = 1;
+
+    private ImageView avatarImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_implicit_intent);
+        avatarImage = findViewById(R.id.image_avatar);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void handleChangeAvatar(View view) {
+
+
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+    }
+}
